97d55ed09f7dcf2f05321ba1ada16cd7e5591701
sadisticaudio <49861376+sadisticaudio@users.noreply.github.com>
2025-09-30 14:17:53 -0500

    Corrected phase calculations after replacing n_phase and n_phase2

--- modular_sum.py
+++ modular_sum.py
@@ -12,13 +12,11 @@ def modular_sum(a,b,freqs, *, out_mags=None, out_phases=None, inout_norms=(3.19,
     if out_phases is None: out_phases = torch.rand([d_model, len(freqs)]) * 2 * np.pi - np.pi
     desired = (out_mags[...,None] * torch.cos(prange * freqs[...,None] * 2 * np.pi / p + out_phases[...,None])).sum(1)
     phzrange = torch.arange(d_model) * 2 * np.pi / d_model
-    # n_phases = torch.arange(d_model) * 2 * np.pi / d_model - np.pi
-    # n_phases2 = ((n_phases+np.pi) * 2) - np.pi
     harmonic1 = torch.cos(prange * freqs[...,None,None] * 2 * np.pi / p + phzrange[...,None] - np.pi)
-    harmonic2 = torch.cos(prange * 2 * freqs[...,None,None] * 2 * np.pi / p + (phzrange * 2)[...,None] - np.pi)
+    harmonic2 = torch.cos(prange * 2 * freqs[...,None,None] * 2 * np.pi / p + 2 * phzrange[...,None] - np.pi)
     vec = (harmonic1 + harmonic2/8) # THIS 1/8 MAGNITUDE RATIO HAS BEEN EMPIRICALLY FOUND WITHOUT PRINCIPLED THEORY
     a_vec, b_vec = vec[...,None], vec[...,None,:]
-    weights = (torch.cos(out_phases - (phzrange[...,None,None] - 0 * np.pi)) * out_mags).permute(2,0,1)
+    weights = (torch.cos(out_phases - 2 * phzrange[...,None,None]) * out_mags).permute(2,0,1)
     ## THE sqrt(2) - 1 BIAS TERM BELOW HAS ALSO BEEN EMPIRICALLY FOUND
     output = einops.einsum(F.relu(a_vec + b_vec + np.sqrt(2) - 1), weights, "freqs d1 ..., freqs d1 d2 -> d2 ...")
     output *= inout_norms[0]/output.square().mean(-1).sqrt().mean()
