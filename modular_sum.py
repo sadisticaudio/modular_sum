4504a288e584fbfb480dc3e8062ad74038aecb7e
sadisticaudio <49861376+sadisticaudio@users.noreply.github.com>
2025-10-04 17:35:02 -0500

    Refactor modular_sum function parameters and logic
    Made the function more general, to allow the number of neurons (N), the number of positions (p), and the d_model dimension (D) to vary.

--- modular_sum.py
+++ modular_sum.py
@@ -3,20 +3,18 @@ import numpy as np
 import einops
 import torch.nn.functional as F
 
-d_model = 128
-p = 113
-prange = torch.arange(p)
-
-def modular_sum(a,b,freqs, *, out_mags=None, out_phases=None, inout_norms=(3.19, 0.101), get_tensors=False):
-    if out_mags is None: out_mags = torch.randn([d_model, len(freqs)]) / np.sqrt(p/2)
-    if out_phases is None: out_phases = torch.rand([d_model, len(freqs)]) * 2 * np.pi - np.pi
-    desired = (out_mags[...,None] * torch.cos(prange * freqs[...,None] * 2 * np.pi / p + out_phases[...,None])).sum(1)
-    phzrange = torch.arange(d_model) * 2 * np.pi / d_model
+def modular_sum(a,b,freqs,p=113,N=8,D=128, *, mags=None, phases=None, inout_norms=(3.19, 0.101), get_tensors=False):
+    prange = torch.arange(p)
+    if mags is not None or phases is not None: D = mags.size(0) if mags is not None else phases.size(0)
+    if mags is None: mags = torch.randn([D, len(freqs)]) / np.sqrt(p/2)
+    if phases is None: phases = torch.rand([D, len(freqs)]) * 2 * np.pi - np.pi
+    desired = (mags[...,None] * torch.cos(prange * freqs[...,None] * 2 * np.pi / p + phases[...,None])).sum(1)
+    phzrange = torch.arange(N) * 2 * np.pi / N
     harmonic1 = torch.cos(prange * freqs[...,None,None] * 2 * np.pi / p + phzrange[...,None] - np.pi)
     harmonic2 = torch.cos(prange * 2 * freqs[...,None,None] * 2 * np.pi / p + 2 * phzrange[...,None] - np.pi)
     vec = (harmonic1 + harmonic2/8) # THIS 1/8 MAGNITUDE RATIO HAS BEEN EMPIRICALLY FOUND WITHOUT PRINCIPLED THEORY
     a_vec, b_vec = vec[...,None], vec[...,None,:]
-    weights = (torch.cos(out_phases - 2 * phzrange[...,None,None]) * out_mags).permute(2,0,1)
+    weights = (torch.cos(phases - 2 * phzrange[...,None,None]) * mags).permute(2,0,1)
     ## THE sqrt(2) - 1 BIAS TERM BELOW HAS ALSO BEEN EMPIRICALLY FOUND
     output = einops.einsum(F.relu(a_vec + b_vec + np.sqrt(2) - 1), weights, "freqs d1 ..., freqs d1 d2 -> d2 ...")
     output *= inout_norms[0]/output.square().mean(-1).sqrt().mean()
